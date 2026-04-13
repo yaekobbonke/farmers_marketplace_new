89690b27c2e2bee43127875cc46239515cd806ab
yaekobbundure <jackmanbonke@gmail.com>
2026-04-14 01:38:22 +0300

    axios instance updated for production

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,19 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  // Use the environment variable if it exists, otherwise fallback to Render
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://farmers-marketplace-twy3.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
