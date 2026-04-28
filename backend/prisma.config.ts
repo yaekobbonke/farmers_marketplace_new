bea1716da9987a4627130c5d045c81ed29ac531e
yaekobbundure <jackmanbonke@gmail.com>
2026-04-28 07:07:35 +0300

    prismaconfig file updated for production

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,17 +1,5 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+// Change this:
+schema: 'prisma/schema.prisma', 
 
-declare const process: { env: { DATABASE_URL?: string } };
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-
-  migrations: {
-    seed: 'ts-node --project tsconfig.json ./prisma/seed.ts',
-    // seed: 'bun backend/prisma/seed.ts',
-  },
-});
+// To this:
+schema: './prisma/schema.prisma',
