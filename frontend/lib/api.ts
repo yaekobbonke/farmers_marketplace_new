73db18b901c46f9854bc2fd5ac35b222e250c54c
yaekobbundure <jackmanbonke@gmail.com>
2026-05-14 03:27:15 +0300

    API URL changed to production url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://farmers-marketplace-twy3.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
